72e95af54efa7bde2a4836fbe0b45256af33968a
Gery Staneva <gerystst@yahoo.com>
2024-08-26 14:30:27 +0300

    print board functions implementation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void sayHi() {
-    cout << "Hi" << endl;
+void lineOne() {
+    cout << "+---+---+---+" << endl;
+}
+
+void lineTwo() {
+    cout << "|   |   |   |" << endl;
 }
 
 int main () {
-    
-    sayHi();
+    for (int i=0; i<3; i++) {
+        lineOne();
+        lineTwo();   
+    }
+    lineOne();
     return 1;
 }
